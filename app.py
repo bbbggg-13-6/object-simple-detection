1df5d212c9b1eb0260533c21b7e3c9fa1df5a749
bbbggg-13-6 <notimportant6200@gmail.com>
2026-07-25 15:23:57 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 
+import sys
+sys.path.append('./ultralytics/yolo')
+
+from utils.checks import check_requirements
+
 model = YOLO("yolov8n.pt")
 
 st.title("YOLO Detection")
